1afa99aed30331c38e8f44d73f297fcbcf2efac7
sebastian <sebaprogramer@gmail.com>
2018-03-02 01:53:27 -0300

    archivo gulp con codigo reparado, ahora funciona perfecto

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
+
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles'), function(){
- gulp
-    .src('index.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('public'))
-}
+gulp.task('styles', function(){
+return gulp.src('index.scss')
+.pipe(sass().on('error', sass.logError)) 
+.pipe(gulp.dest('public'))
+});
 
 gulp.task('default', ['styles'])
